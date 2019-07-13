ce184459d8fdcdac8e0a733a1f97e372ecacc317
Amanda King <amapickell@gmail.com>
2019-07-13 12:47:33 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,27 @@
 class Post
-  attr_accessor
+  attr_accessor :title
+  @@all = []
   
+  def initialize (title)
+    @title = title
+    @@all << self
+  end
   
+  def self.all
+    @@all
+  end
+  
+  def title
+    @title
+  end
+  
+  def author
+    self.author
+  end
+  
+  def author_name
+    return nil if !self.author
+    self.author.name
+  end
   
 end
